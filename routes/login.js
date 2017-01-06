11cdf1ed2bc8db176dbfcafbcfb41aec16ec122f
Tal Binder <me@talbinder.com>
2017-01-06 03:24:33 +0200

    Fixed Facebook permissions

--- routes/login.js
+++ routes/login.js
@@ -9,15 +9,17 @@ function loginRoutes(app, passport) {
     // Redirect the user to Facebook for authentication.  When complete,
     // Facebook will redirect the user back to the application at
     //   /auth/facebook/callback
-    app.get('/auth/facebook', passport.authenticate('facebook',  { scope: 'email' }));
+    app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['public_profile', 'email']}));
 
     // Facebook will redirect the user to this URL after approval.  Finish the
     // authentication process by attempting to obtain an access token.  If
     // access was granted, the user will be logged in.  Otherwise,
     // authentication has failed.
     app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', { successRedirect: '/',
-            failureRedirect: '/' }));
+        passport.authenticate('facebook', {
+            successRedirect: '/',
+            failureRedirect: '/'
+        }));
 
     //Twitter
 
