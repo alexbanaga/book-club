7582069171c9356dbfb40fb4aa4707de2163512f
Tal Binder <me@talbinder.com>
2017-01-06 02:22:39 +0200

    Debug print

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.all('*', function(req, res) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    debug(err);
+    debug(err.stack);
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
